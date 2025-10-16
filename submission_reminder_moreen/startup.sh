feb0e641ccbc44f5030b86f35864d4eb5579c356
Moreenmm <m.murugi@alustudent.com>
2025-10-16 16:40:01 +0200

    deletion of submission_reminder_moreen file

--- submission_reminder_moreen/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# create a script that runs the reminder app
-
-if [ ! -f "config/config.env" ]; then
-    echo "Error! The config.env does not exist and cannot start the reminder app"
-    exit 1
-fi
-
-./app/reminder.sh
